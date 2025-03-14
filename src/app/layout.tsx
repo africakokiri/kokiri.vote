48b04e9e03c2264706b10ed8ac8ec668f1dadc29
Choi Chan Hwan <africakokiri@gmail.com>
2025-03-15 00:21:15 +0900

    Metadata 작성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,11 @@ import "@/styles/globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: ""
+  title: "코끼리",
+  description: "온라인 여론조사 및 투표 플랫폼",
+  icons: {
+    icon: "/icons/kokiri.svg"
+  }
 };
 
 export default function RootLayout({
