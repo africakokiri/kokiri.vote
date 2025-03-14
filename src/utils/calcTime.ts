0ba3d6ff9121ecd824290af7443a7d42af9d9fa3
Choi Chan Hwan <africakokiri@gmail.com>
2025-03-15 02:13:49 +0900

    날짜 계산 함수 formatExpirationTime 추가

---
+++ src/utils/calcTime.ts
@@ -0,0 +1,23 @@
+import dayjs from "dayjs";
+
+export const formatExpirationTime = (expirationDate: string) => {
+  const now = dayjs(); // 현재 시간
+  const expiration = dayjs(expirationDate); // expires_at
+
+  const timeDiff = expiration.diff(now, "minute"); // 차이를 분 단위로 계산
+
+  if (timeDiff < 1440) {
+    // 24시간 미만 (1440분)
+    const hours = expiration.diff(now, "hour");
+    const minutes = expiration.diff(now, "minute") % 60;
+
+    if (hours < 1) {
+      return `${minutes}분`;
+    } else {
+      return `${hours}시간 ${minutes}분`;
+    }
+  } else {
+    const days = expiration.diff(now, "day");
+    return `${days}일`;
+  }
+};
