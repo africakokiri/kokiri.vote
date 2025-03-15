77c909a2fc8f9feb1bb4ed8db7264625cc7512ed
Choi Chan Hwan <africakokiri@gmail.com>
2025-03-15 09:33:37 +0900

    Category 객체 enum화
    enum은 사용하지 않음

--- src/constants/category.ts
+++ src/constants/category.ts
@@ -1,1 +1,10 @@
-export type Category = "전체" | "사회" | "정치" | "문화" | "기타";
+export const CATEGORY = {
+  전체: 0,
+  사회: 1,
+  정치: 2,
+  문화: 3,
+  이슈: 4,
+  기타: 5
+} as const;
+
+type CATEGORY = (typeof CATEGORY)[keyof typeof CATEGORY];
