babb5a6110c41f6f7f2f226700ab83bcdac2f0d6
Choi Chan Hwan <africakokiri@gmail.com>
2025-03-28 12:09:55 +0900

    Metadata 작성, Header 컴포넌트 UI 구성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
+import { Header } from "@/components/Header";
 import NextThemesProvider from "@/libs/next-themes/next-themes-provider";
 import "@/styles/globals.css";
 
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: ""
+  title: "코끼리",
+  description: "온라인 여론조사 및 투표 플랫폼",
+  icons: {
+    icon: "/icons/kokiri.svg"
+  }
 };
 
 export default function RootLayout({
@@ -15,7 +20,10 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className="h-screen w-screen antialiased">
-        <NextThemesProvider>{children}</NextThemesProvider>
+        <NextThemesProvider>
+          <Header />
+          {children}
+        </NextThemesProvider>
       </body>
     </html>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import { AlignJustify } from "lucide-react";
+
+export const Header = () => {
+  return (
+    <header
+      className="fixed flex w-full items-center justify-between
+border-b-[1px] border-black/20 px-4 py-3 backdrop-blur-md"
+    >
+      <h1 className="text-lg font-extrabold">코끼리</h1>
+      <AlignJustify className="h-6 w-6" />
+    </header>
+  );
+};
