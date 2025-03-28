6765f09aa1e6158b9c18a25e82f3fa2830b2c015
Choi Chan Hwan <africakokiri@gmail.com>
2025-03-28 13:08:17 +0900

    카테고리 분류 기능 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -16,12 +18,14 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
+import { CATEGORY } from "@/constants/category";
 import { cn } from "@/libs/shadcn/utils";
 import { MOCK_VOTE_LIST } from "@/mock/vote_list";
 import { calcRemainDate } from "@/utils/calcRemainDate";
 
 import { ChevronRight, Clock, Filter, Plus, Users } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
 
 const MOCK_DATA_EXPIRES_AT_ASC = MOCK_VOTE_LIST.sort((a, b) => {
   if (a.expires_at > b.expires_at) return 1;
@@ -30,7 +34,11 @@ const MOCK_DATA_EXPIRES_AT_ASC = MOCK_VOTE_LIST.sort((a, b) => {
   return 0;
 });
 
-export default function page() {
+export default function MainPage() {
+  const [selectedCategory, setSelectedCategory] = useState<
+    "전체" | number
+  >("전체");
+
   return (
     <div className="space-y-4 px-4">
       <div className="flex items-center justify-between">
@@ -62,79 +70,94 @@ border-black/20 p-3"
               </DropdownMenuLabel>
               <DropdownMenuSeparator />
               <DropdownMenuRadioGroup className="space-y-2">
-                <DropdownMenuRadioItem value="전체">
+                <DropdownMenuRadioItem
+                  onSelect={() => setSelectedCategory("전체")}
+                  value="전체"
+                >
                   전체
                 </DropdownMenuRadioItem>
-                {/* {Object.entries(CATEGORY).map(([item]) => {
+                {Object.entries(CATEGORY).map(([key, value]) => {
                   return (
                     <DropdownMenuRadioItem
-                      key={item}
-                      value={item}
-                      className="flex justify-center"
+                      key={key}
+                      value={key}
+                      onSelect={() => setSelectedCategory(value)}
                     >
-                      {item}
+                      {key}
                     </DropdownMenuRadioItem>
                   );
-                })} */}
+                })}
               </DropdownMenuRadioGroup>
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
       </div>
       {MOCK_DATA_EXPIRES_AT_ASC.map(
-        ({ id, title, description, votes_count, expires_at }) => {
+        ({
+          id,
+          title,
+          description,
+          votes_count,
+          expires_at,
+          category
+        }) => {
           const remainDate = calcRemainDate(expires_at);
 
-          return (
-            <Card
-              key={id}
-              className="h-full"
-            >
-              <CardHeader>
-                <CardTitle className="line-clamp-1 text-xl">
-                  {title}
-                </CardTitle>
-                <CardDescription className="line-clamp-2">
-                  {description}
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <div
-                  className="flex items-center gap-4 text-sm
-text-muted-foreground"
-                >
-                  <div className="flex items-center gap-1">
-                    <Users className="h-4 w-4" />
-                    <span>{votes_count}명</span>
-                  </div>
+          if (
+            selectedCategory === category ||
+            selectedCategory === "전체"
+          ) {
+            return (
+              <Card
+                key={id}
+                className="h-full"
+              >
+                <CardHeader>
+                  <CardTitle className="line-clamp-1 text-xl">
+                    {title}
+                  </CardTitle>
+                  <CardDescription className="line-clamp-2">
+                    {description}
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
                   <div
-                    className={cn(
-                      "flex items-center gap-1",
-                      remainDate.includes("분 남음") && "text-red-500"
-                    )}
+                    className="flex items-center gap-4 text-sm
+text-muted-foreground"
                   >
-                    <Clock className="h-4 w-4" />
-                    <span>{remainDate}</span>
+                    <div className="flex items-center gap-1">
+                      <Users className="h-4 w-4" />
+                      <span>{votes_count}명</span>
+                    </div>
+                    <div
+                      className={cn(
+                        "flex items-center gap-1",
+                        remainDate.includes("분 남음") && "text-red-500"
+                      )}
+                    >
+                      <Clock className="h-4 w-4" />
+                      <span>{remainDate}</span>
+                    </div>
                   </div>
-                </div>
-              </CardContent>
-              <CardFooter>
-                <Link
-                  href={`/vote/${id}`}
-                  className="ml-auto"
-                >
-                  <Button
-                    variant="ghost"
-                    className="gap-1"
-                    aria-label="투표하기"
+                </CardContent>
+                <CardFooter>
+                  <Link
+                    href={`/vote/${id}`}
+                    className="ml-auto"
                   >
-                    투표하기
-                    <ChevronRight className="h-4 w-4" />
-                  </Button>
-                </Link>
-              </CardFooter>
-            </Card>
-          );
+                    <Button
+                      variant="ghost"
+                      className="gap-1"
+                      aria-label="투표하기"
+                    >
+                      투표하기
+                      <ChevronRight className="h-4 w-4" />
+                    </Button>
+                  </Link>
+                </CardFooter>
+              </Card>
+            );
+          }
         }
       )}
     </div>
