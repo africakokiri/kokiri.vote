ed32e0500bf82b950e6f54b40576728c20ea94cb
Choi Chan Hwan <africakokiri@gmail.com>
2025-03-28 12:56:15 +0900

    Category에서 '전체' 제거

--- src/constants/category.ts
+++ src/constants/category.ts
@@ -1,11 +1,10 @@
 export const CATEGORY = {
-  전체: 0,
-  사회: 1,
-  정치: 2,
-  문화: 3,
-  이슈: 4,
-  기타: 5,
-  기술: 6
+  사회: 0,
+  정치: 1,
+  문화: 2,
+  이슈: 3,
+  기타: 4,
+  기술: 5
 } as const;
 
 type CATEGORY = (typeof CATEGORY)[keyof typeof CATEGORY];
